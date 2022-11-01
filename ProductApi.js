967f88bc396d9b990b7b5e1eb2dc5f0e1f19b09d
Nikhil Oberoi <91828301+Nikhil5997@users.noreply.github.com>
2022-11-01 18:01:31 +0530

    Add files via upload

---
+++ ProductApi.js
@@ -0,0 +1,47 @@
+import React,{useState,useEffect} from 'react'
+
+
+const ProductApi=()=>{
+  const url =  'https://dummyjson.com/products';
+  const [data,setData] = useState([])
+
+  useEffect(()=>{
+    fetch(url).then(response=>response.json()).then(json=>{
+      console.log(json)
+     setData(json.products)
+    }).catch((error)=>{
+      console.log(error)
+    })
+  },[])
+  return(
+    <div className='main-container'><h1>ProductApi</h1>
+    <div className='grid-wrapper'>
+   
+    {
+      data.map((item,index)=>{
+        return(<div key={item.index}>
+         {/* <div className='cardp'>{index+1}</div>*/}
+         
+         <div className='proimg'><img src={item.images[2]}/></div>
+          <div className='protittle'><h2>{item.title}</h2></div>
+          <div className='proprice'><h3>Price : {item.price}</h3></div>
+          <div className='probrand'><h4>{item.brand}</h4></div>
+          <button type='submit'>Add to card</button>
+          
+          
+  
+
+ 
+
+
+          
+          </div>)
+      })
+    }
+    </div>
+    
+    
+    </div>)
+}
+
+export default ProductApi;
