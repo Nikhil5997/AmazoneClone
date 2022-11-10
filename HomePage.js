9260bc7023bfc8a7f74af2e8e6517e532abc1a12
Nikhil Oberoi <91828301+Nikhil5997@users.noreply.github.com>
2022-11-10 21:59:30 +0530

    Update HomePage.js

--- HomePage.js
+++ HomePage.js
@@ -1,10 +1,25 @@
 import React from 'react'
+import { Outlet, Link } from "react-router-dom";
 
 const HomePage = () => {
   return (
-    <div>
-      
-    </div>
+    <div className="Navbar">
+   
+      <nav>
+     
+        <Link to="/">Home</Link>
+        
+
+
+        <Link to="/productapi">ProductApi</Link>
+
+        <Link to="/login">Login</Link>
+
+        <Link to="/addtocard">AddtoCard</Link>
+  
+
+      </nav>
+      <Outlet />
   )
 }
 
